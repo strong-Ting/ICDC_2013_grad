1b5daf9b2491902a9347f8b231534b576328cd23
strong-Ting <s8527502987@gmail.com>
2020-10-06 10:48:31 +0800

    use compile_ultra to get better result,it has timing violation,but pass the test

--- sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++ sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -1,5 +1,5 @@
 1600251382 /home/lin/github/tsmc13_neg.v
-1601869552 /home/lin/github/CIC_2013_grad/sim/testfixture2.v
-1601869457 /home/lin/github/CIC_2013_grad/syn/FAS_syn.v
+1601950605 /home/lin/github/CIC_2013_grad/sim/testfixture2.v
+1601951591 /home/lin/github/CIC_2013_grad/syn/FAS_syn.v
 1601869559 /home/lin/github/CIC_2013_grad/sim/testfixture1.v
 1601867019 /home/lin/github/CIC_2013_grad/src/FAS.v
--- syn/DC_syn.tcl
+++ syn/DC_syn.tcl
@@ -1,7 +1,9 @@
 
-set_host_options -max_cores 6
+set_host_options -max_cores 4
 #Read All Files
-read_verilog ../src/FAS.v
+#read_verilog ../src/FAS.v
+analyze -format verilog ../src/FAS.v
+elaborate FAS
 current_design FAS
 link
 
@@ -9,8 +11,10 @@ link
 source -echo -verbose FAS.sdc
 
 #Synthesis all design
-compile -map_effort medium -area_effort medium
-compile -map_effort medium -area_effort medium -inc
+#compile -map_effort medium -area_effort medium
+#compile -map_effort medium -area_effort medium -inc
+compile_ultra -retime
+compile_ultra -inc
 
 write -format ddc     -hierarchy -output "FAS_syn.ddc"
 write_sdf FAS_syn.sdf
