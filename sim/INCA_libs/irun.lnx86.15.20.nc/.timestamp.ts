c11807e14b9ccf00e42745deda21d90d9b88a16b
strong-Ting <s8527502987@gmail.com>
2020-11-24 11:39:27 +0800

    change fft architecture

--- sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
+++ sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -2,4 +2,4 @@
 1606036832 /home/lin/github/CIC_2013_grad/sim/testfixture2.v
 1606037220 /home/lin/github/CIC_2013_grad/syn/FAS_syn.v
 1606036816 /home/lin/github/CIC_2013_grad/sim/testfixture1.v
-1606037302 /home/lin/github/CIC_2013_grad/src/FAS.v
+1606189004 /home/lin/github/CIC_2013_grad/src/FAS.v
--- sim/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++ sim/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -1,6 +1,6 @@
 1600251382 /home/lin/github/tsmc13_neg.v
 1606036816 /home/lin/github/CIC_2013_grad/sim/testfixture1.v
-1606037302 /home/lin/github/CIC_2013_grad/src/FAS.v
+1606189004 /home/lin/github/CIC_2013_grad/src/FAS.v
 1606036832 /home/lin/github/CIC_2013_grad/sim/testfixture2.v
-1606037220 /home/lin/github/CIC_2013_grad/syn/FAS_syn.v
-1606037302 /home/lin/github/CIC_2013_grad/src
+1606037907 /home/lin/github/CIC_2013_grad/syn/FAS_syn.v
+1606189004 /home/lin/github/CIC_2013_grad/src
